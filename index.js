6622756e3b87e43c081a71473fb70946c74f67e2
taiku <31465218+taikutsunan@users.noreply.github.com>
2020-09-23 20:43:39 -0400

    what the fuck why is the month offset by 1 what why

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ module.exports = class CustomTimestamps extends Plugin {
 
       // dunno how good this solution is but whatevs
       var timestampFormats = [
-        ["%0M", timestampObject.getMonth() < 10 ? "0" + timestampObject.getMonth() : timestampObject.getMonth()],
+        ["%0M", timestampObject.getMonth() + 1 < 10 ? "0" + timestampObject.getMonth() + 1 : timestampObject.getMonth() + 1],
         ["%0D", timestampObject.getDate() < 10 ? "0" + timestampObject.getDate() : timestampObject.getDate()],
         ["%0h", timestampObject.getHours() < 10 ? "0" + timestampObject.getHours() : timestampObject.getHours()],
         ["%0H", (timestampObject.getHours() % 12 || 12) < 10 ? "0" + (timestampObject.getHours() % 12 || 12) : (timestampObject.getHours() % 12 || 12)],
         ["%0m", timestampObject.getMinutes() < 10 ? "0" + timestampObject.getMinutes() : timestampObject.getMinutes()],
         ["%0s", timestampObject.getSeconds() < 10 ? "0" + timestampObject.getSeconds() : timestampObject.getSeconds()],
         ["%Y", timestampObject.getFullYear()],
-        ["%M", timestampObject.getMonth()],
+        ["%M", timestampObject.getMonth() + 1],
         ["%D", timestampObject.getDate()],
         ["%h", timestampObject.getHours()],
         ["%H", (timestampObject.getHours() % 12 || 12)],
