4cfaddc422cc5ab2cc5083b0a11adb2f076a7645
taiku <31465218+taikutsunan@users.noreply.github.com>
2020-09-27 16:28:17 -0400

    add %g variable (how long ago)

--- timestamp.js
+++ timestamp.js
@@ -1,8 +1,11 @@
 const vars = require("./variables.json");
+
+
 exports.parseTimestamp = (timestamp, timestampSchematic) => {
+    var now = new Date()
     vars.forEach(v => {
       if (timestampSchematic.includes(`%${v.selector}`)) {
-        timestampSchematic = timestampSchematic.replace(`%${v.selector}`, eval(v.eval))
+        timestampSchematic = timestampSchematic.split(`%${v.selector}`).join(eval(v.eval))
       }
     })
     return timestampSchematic
